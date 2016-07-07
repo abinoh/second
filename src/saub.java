70256822b4f67b0552c56d5a6ae53986bcf7fd8e
abin <abininbox@gmail.com>
2016-07-08 00:45:24 +0530

    Operators ops

---
+++ src/saub.java
@@ -0,0 +1,18 @@
+/**
+ * Created by abin on 8/7/16.
+ */
+public class saub {
+    public static void main(String[] args) {
+        int i = 10;
+        i +=2;
+        System.out.println(i);
+        i -=2;
+        System.out.println(i);
+        i *=2;
+        System.out.println(i);
+        i /=2;
+        System.out.println(i);
+        i %=2;
+        System.out.println(i);
+    }
+}
