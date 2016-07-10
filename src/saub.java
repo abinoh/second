f2f29d606b4678d048326437e1dd009a403d475a
abin <abininbox@gmail.com>
2016-07-10 20:04:59 +0530

    First methord

--- src/saub.java
+++ src/saub.java
@@ -2,21 +2,16 @@
 /**Grade Ranks**/
 public class saub {
     public static void main(String[] args) {
-        int grade = 6;
-        if (grade > 9 && grade == 10) {
-            System.out.println("Excellent");
-        } else if (grade > 8 && grade <= 9) {
-            System.out.println("Very Good");
-        } else if (grade > 7 && grade <= 8) {
-            System.out.println("Good");
-        } else if (grade > 6 && grade <= 7) {
-            System.out.println("Satisfacory Work");
-        } else if (grade > 4 && grade <= 6) {
-            System.out.println("Sufficient");
-        } else if (grade > 3 && grade <= 4) {
-            System.out.println("Not Sufficient");
-        } else if (grade < 3) {
-            System.out.println("Unfair Behavior");
+        int no=10;
+        System.out.println("Here we go "+ no);
+        make(no);
+        System.out.println("and "+ no);
+
+
         }
+    public static void make(int no){
+        no=11;
+
+
     }
 }
