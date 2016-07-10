22d9ba9a6fa4754718f5a3cfda6da1ef6fda4862
abin <abininbox@gmail.com>
2016-07-10 16:36:38 +0530

    Expression If else Grade points

--- src/saub.java
+++ src/saub.java
@@ -1,15 +1,22 @@
-/**
- * Created by abin on 8/7/16.
- */
+/*** Created by abin on 8/7/16.*/
+/**Grade Ranks**/
 public class saub {
     public static void main(String[] args) {
-        double kilo=(100*1.00);
-        int hig=50;
-        if (hig==50){
-            System.out.println("This is an expression");
-            System.out.println(+kilo);
+        int grade = 6;
+        if (grade > 9 && grade == 10) {
+            System.out.println("Excellent");
+        } else if (grade > 8 && grade <= 9) {
+            System.out.println("Very Good");
+        } else if (grade > 7 && grade <= 8) {
+            System.out.println("Good");
+        } else if (grade > 6 && grade <= 7) {
+            System.out.println("Satisfacory Work");
+        } else if (grade > 4 && grade <= 6) {
+            System.out.println("Sufficient");
+        } else if (grade > 3 && grade <= 4) {
+            System.out.println("Not Sufficient");
+        } else if (grade < 3) {
+            System.out.println("Unfair Behavior");
         }
-
     }
-
 }
