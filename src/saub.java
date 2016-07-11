5de996d42ad46e11ad26fea69963e1c5ec6c5c72
abin <abininbox@gmail.com>
2016-07-11 23:52:54 +0530

    Metords simple

--- src/saub.java
+++ src/saub.java
@@ -1,17 +1,13 @@
-/*** Created by abin on 8/7/16.*/
-/**Grade Ranks**/
-public class saub {
-    public static void main(String[] args) {
-        int no=10;
-        System.out.println("Here we go "+ no);
-        make(no);
-        System.out.println("and "+ no);
+/**
+ * Created by abin on 11/7/16.
+ */
+/**Changed to Lynda**/
 
-
-        }
-    public static void make(int no){
-        no=11;
-
-
+public class javing {
+    public static void main(String[] args){
+        dosome();
+    }
+    private static void dosome(){
+        System.out.println("Hai here");
     }
 }
